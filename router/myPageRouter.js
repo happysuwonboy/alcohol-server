d7b3c19437b122e44fc428dcd1697e2f5bcad559
최유진 <tneod2@icloud.com>
2024-01-08 16:27:52 +0900

    [Fix]

--- router/myPageRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as controller from '../controller/myPageController.js';
-
-const router = express.Router();
-
-router.get('/review/:userid', controller.getMyReview);
-
-export default router;
