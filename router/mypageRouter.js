269587c494e1d328ebaebf3c5cb6f6e89ced046b
syh <yhoonsong96@gmail.com>
2024-01-08 16:46:39 +0900

    [Fix] 파일명 충돌 현상 해결

---
+++ router/mypageRouter.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as mypageController from '../controller/mypageController.js';
+
+const router = express.Router();
+
+router.get('/review/:userid', mypageController.getMyReview);
+router.get('/order/:userId', mypageController.getOrders);
+
+export default router;
+
