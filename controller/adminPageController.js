a1f95a982d316a5c4be35cd9dc57177f93a303cc
jeongyumin <onewmean@gmail.com>
2024-01-10 16:23:39 +0900

    [ Add ] admin 페이지 서버와 이미지 중복 구현 및 데이터 요청 바인딩
    - adminPageRouter.js
      form 등록 submit 요청 시 food 이미지 파일 이름 그대로 서버에 저장
      alcohol 상품 이미지 유니크 이름 생성 서버 저장 및 데이터 저장
      위한 값 연결 확인
    
      : ImgUploadMiddleware
        food 이미지 및 alcohol 이미지 multer 미들웨어 생성
    
    - adminPageController.js
    
      createProduct
      : form 데이터 연결 확인, 이미지 연결 확인

--- controller/adminPageController.js
+++ controller/adminPageController.js
@@ -13,12 +13,18 @@ export async function getAlcoholList(req, res) {
   const startIndex = endIndex -9;
   const rows = await adminPageRepository.getAlcoholList({startIndex, endIndex});
   res.json(rows);
-}
+};
 
+/**
+ * : 이미지 서버 중복체크 ( 파일명 체크 )
+ * @param {*} req 
+ * @param {*} res 
+ */
 export async function getImgDuplicate(req, res) {
   const {foodImages}  = req.body;
+  console.log(foodImages);
   const serverImages = await fs.readdir('assets/images/food_img');
-
+  console.log(serverImages);
   // 중복 체크
   const duplicateImages = [];
   foodImages.forEach((name, idx) => {
@@ -32,4 +38,15 @@ export async function getImgDuplicate(req, res) {
   } else {
     res.json({error:false, message: '중복 이미지 없음'})
   };
+};
+
+export async function createProduct(req, res) {
+  const productForm = req.body;
+  console.log(req.files);
+  const foodImg = req.files?.food_img || [];
+  const alcohol = req.files?.alcohol_img || [];
+  
+  console.log(productForm);
+  console.log(alcohol);
+  console.log(foodImg)
 }
--- router/adminPageRouter.js
+++ router/adminPageRouter.js
@@ -2,10 +2,47 @@ import * as adminPageController from '../controller/adminPageController.js';
 import express  from 'express';
 import multer from 'multer';
 import path from 'path';
+import uuid from 'uuid4';
 
 const router = express.Router();
 
+const ImgUpload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      let uploadPath = path.join(path.resolve(), 'assets', 'images');
+
+      // 조건문을 사용하여 필드에 따라 저장 경로 결정
+      if (file.fieldname === 'alcohol_img') {
+        uploadPath = path.join(uploadPath, 'alcohol_img');
+      } else if (file.fieldname === 'food_img') {
+        uploadPath = path.join(uploadPath, 'food_img');
+      }
+      done(null, uploadPath);
+    },
+    filename(req, file, done) {
+      let fileName;
+
+      // 조건문을 사용하여 필드에 따라 파일 이름 결정
+      if (file.fieldname === 'alcohol_img') {
+        const randomId = uuid();
+        const ext = path.extname(file.originalname);
+        fileName = randomId + ext;
+      } else if (file.fieldname === 'food_img') {
+        file.originalname = Buffer.from(file.originalname, 'latin1').toString(
+          'utf8',
+          );
+        fileName = file.originalname;
+      }
+      done(null, fileName);
+    }
+  })
+});
+
+const ImgUploadMiddleware = ImgUpload.fields([{ name: 'food_img', maxCount: 3 }, { name: 'alcohol_img', maxCount: 3 }]);
+
+
 router.get('/product/:page', adminPageController.getAlcoholList);
+router.post('/product', ImgUploadMiddleware, adminPageController.createProduct);
 router.post('/imgduplicate', adminPageController.getImgDuplicate);
 
 export default router;
