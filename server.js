e73f2e137990d88745d55b4f7cee3925ef458d20
DESKTOP-H6B44QU\TJ <yhoonsong96@gmail.com>
2024-01-16 11:26:42 +0900

    [Refactor] cors 허용 오리진, mysql config 환경 변수에서 관리하도록 변경

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = 8000;
 
 server.use(
   cors({
-    origin: [`http://localhost:3000`, `http://localhost:3001`],
+    origin: JSON.parse(process.env.FRONTEND_ORIGINS),   
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
